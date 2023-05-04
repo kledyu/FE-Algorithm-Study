6641f0a17546aa894fbfd88887bca66e65b7142e
junehee <dev.junehee@gmail.com>
2023-05-04 21:19:12 +0900

    [PGM] 문자열을 정수로 바꾸기 / Level 1 / 10초

---
+++ junehee/문자열을정수로바꾸기.js
@@ -0,0 +1,18 @@
+// ver.1
+function solution(s) {
+    var answer = 0 + Number(s);
+    return answer;
+}
+
+// ver.2
+function solution(s) {
+	return s * 1;
+}
+
+function solution(s) {
+	return s / 1;
+}
+
+function solution(s) {
+	return s - 0;
+}
