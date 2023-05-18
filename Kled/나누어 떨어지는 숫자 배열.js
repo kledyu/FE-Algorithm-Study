186f1ab07cf0f19c0f453c7ea97159f42626a7ab
Kled Yu <83483378+1017yu@users.noreply.github.com>
2023-05-18 10:22:17 +0900

    [PGM] 나누어 떨어지는 숫자 배열 / Level 1 / 5분

---
+++ Kled/나누어 떨어지는 숫자 배열.js
@@ -0,0 +1,7 @@
+const solution = (arr, divisor) => {
+    let answer = [];
+    answer = arr
+        .filter(v => v % divisor === 0)
+        .sort((a, b) => a - b)
+    return answer.length ? answer : [-1]
+}
