11a3af063b6ce2575cbe7cc600c209019c2525e1
junehee <dev.junehee@gmail.com>
2023-06-08 18:12:31 +0900

    [PGM] 숫자 문자열과 영단어 / Level 1 / 30분

---
+++ junehee/숫자문자열과영단어.js
@@ -0,0 +1,13 @@
+function solution(s) {
+  const numbers = {
+    "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4,
+    "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9
+  }
+  
+  for (let [key, value] of Object.entries(numbers)) {
+    let regExp = new RegExp(key, 'g')
+    s = s.replace(regExp, value)
+  }
+  
+  return +s
+}
