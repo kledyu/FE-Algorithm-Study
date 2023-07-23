881a80d5a2cf7c5299dbddf0dcf5bbe4c4cb26cb
Kled Yu <83483378+1017yu@users.noreply.github.com>
2023-07-23 23:42:38 +0900

    [PGM] 같은 숫자는 싫어 / Level 1 / 10분

---
+++ Kled/같은 숫자는 싫어.js
@@ -0,0 +1,9 @@
+function solution(arr) {
+    let stack = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== arr[i + 1]) {
+            stack.push(arr[i])
+        }
+    }
+    return stack
+}
