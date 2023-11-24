73fb133739ad9a345eb3001b662d0cdd0056e79b
Dongmin <122417731+cdm1263@users.noreply.github.com>
2023-11-24 18:36:11 +0900

    [PGM] 문자열 나누기 / Level 1 / 15분

---
+++ dongmin/[PGM] 문자열 나누기 Level 1 15분.js
@@ -0,0 +1,18 @@
+ function solution(keymap, targets) {
+    const answer = Array.from({ length: targets.length });
+    targets.forEach((target, idx) => {
+      let count = 0;
+      for (let i = 0; i < target.length; i++) {
+        const indexArray = keymap.map((alp) => alp.indexOf(target[i]));
+        if (Math.max(...indexArray) === -1) {
+          count = -1;
+          break;
+        } else {
+          const minIndex = Math.min(...indexArray.filter((v) => v >= 0)) + 1;
+          count = count + minIndex;
+        }
+      }
+      answer[idx] = count;
+    });
+    return answer;
+  }
