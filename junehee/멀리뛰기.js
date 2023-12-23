dc96782a9a60572b36fa48bc1031cd5fc54f4f9d
JuneHee <dev.junehee@gmail.com>
2023-12-23 21:00:23 +0900

    [PGM] 멀리 뛰기 / Level 2 / 1시간

---
+++ junehee/멀리뛰기.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    let arr = [1, 1];
+    
+    for (let i = 2; i <= n; i += 1) {
+        arr.push((arr[i - 1] + arr[i - 2]) % 1234567);
+    }
+    
+    return arr[n];
+}
