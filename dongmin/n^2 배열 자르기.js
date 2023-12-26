6311bdb9571bcde730dae77abc243dd4ab31a7e7
Dongmin <122417731+cdm1263@users.noreply.github.com>
2023-12-26 14:14:44 +0900

    Update n^2 배열 자르기.js

--- dongmin/n^2 배열 자르기.js
+++ dongmin/n^2 배열 자르기.js
@@ -2,16 +2,20 @@ function solution(n, left, right) {
   const arr = [];
 
   // left부터 right까지 구성
-  // x축은 몫, y축은 나머지
+  // x좌표는 몫, y좌표는 나머지
   for (let i = left; i <= right; i += 1) {
     const x = Math.trunc(i / n);
     const y = i % n;
 
+    // y좌표의 값이 x좌표의 값과 같거나 크면
+    // y + 1 을 push (index는 0부터 시작하므로 1을 더함)
     if (x <= y) {
       arr.push(y + 1);
       continue;
     }
 
+    // y좌표의 값이 x좌표의 값보다 작으면
+    // x + 1을 push (index는 0부터 시작하므로 1을 더함)
     arr.push(x + 1);
   }
 
