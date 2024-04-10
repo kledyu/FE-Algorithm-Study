ac5b119edaf742f2e1f1293c28082058b486eef4
JuneHee <dev.junehee@gmail.com>
2024-04-10 12:42:42 +0900

    [PGM] 멀리 뛰기 / Level 2 / 1시간+@

--- junehee/멀리뛰기.js
+++ junehee/멀리뛰기.js
@@ -7,3 +7,20 @@ function solution(n) {
     
     return arr[n];
 }
+
+
+// 재귀
+function solution(n) {
+    return recursion(n);
+}
+
+function recursion(n, stack = {}) {
+    if (n === 1) return 1;
+    if (n === 2) return 2;
+    if (stack[n]) return stack[n];
+    
+    const count = recursion(n - 1, stack) + recursion(n - 2, stack);
+    stack[n] = count; 
+    
+    return count % 1234567;
+}
